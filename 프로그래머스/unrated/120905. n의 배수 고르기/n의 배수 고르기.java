e191060cf98d1994cbd632badadc7d51a4954962
hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2024-01-10 04:47:43 +0900

    [level unrated] Title: n의 배수 고르기, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = new int[numlist.length];
+        int index = 0;
+
+        for (int i = 0; i < numlist.length; i++) {
+            if (numlist[i] % n == 0) {
+                answer[index++] = numlist[i];
+            }
+        }
+
+        return Arrays.copyOf(answer, index);
+    }
+}
