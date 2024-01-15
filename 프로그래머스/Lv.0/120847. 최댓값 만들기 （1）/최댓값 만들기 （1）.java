87eae5038737a8a5e3001a12235f5f960cb44a79
hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2024-01-15 21:53:08 +0900

    [level Lv.0] Title: 최댓값 만들기 (1), Time: 0.49 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/Lv.0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,12 +1,8 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
-        for(int i = 0; i < numbers.length; i++){
-            Arrays.sort(numbers);
-            answer[i] = numbers[i];
-        }
-        int lastIndex = answer.length - 1;
-        return answer[lastIndex] * answer[lastIndex - 1];
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
     }
 }
