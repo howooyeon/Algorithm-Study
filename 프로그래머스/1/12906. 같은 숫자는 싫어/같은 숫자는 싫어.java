700a51cf06daf834b2400e52d34d27cdca82e3ac
hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2024-02-19 23:47:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.79 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,37 +2,22 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int[] arr) {
-        ArrayList<Integer> resultList = new ArrayList<>(); // 결과를 저장할 ArrayList
+        ArrayList<Integer> resultList = new ArrayList<>();
         
-        int prev = -1; // 이전 숫자를 저장할 변수, 초기값은 배열 arr의 범위에 속하지 않는 값으로 설정
+        int prev = -1; 
         
         for (int num : arr) {
-            if (num != prev) { // 현재 숫자가 이전 숫자와 다를 때만 결과에 추가
+            if (num != prev) {
                 resultList.add(num);
-                prev = num; // 현재 숫자를 이전 숫자로 업데이트
+                prev = num;
             }
         }
         
-        // ArrayList를 배열로 변환하여 반환
         int[] answer = new int[resultList.size()];
         for (int i = 0; i < resultList.size(); i++) {
             answer[i] = resultList.get(i);
         }
         
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        // 예시 배열 arr에 대한 테스트
-        int[] arr1 = {1, 1, 3, 3, 0, 1, 1};
-        int[] arr2 = {4, 4, 4, 3, 3};
-
-        int[] result1 = solution.solution(arr1);
-        int[] result2 = solution.solution(arr2);
-
-        System.out.println(Arrays.toString(result1)); // [1, 3, 0, 1]
-        System.out.println(Arrays.toString(result2)); // [4, 3]
     }
 }
