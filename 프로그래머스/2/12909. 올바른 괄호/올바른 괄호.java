dbe08b8ba492d826c497fd8cd2cdda41023f6274
hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2024-02-23 01:22:27 +0900

    [level 2] Title: 올바른 괄호, Time: 20.58 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,17 +1,30 @@
+import java.util.Stack;
+
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        int cnt = 0; 
-        for(int i = 0; i < s.length();i++){ 
-            if(s.charAt(i) == '(') cnt++;
-            else cnt--;
+        boolean answer = true;  
+        String res = "YES";
+        Stack<Integer> st = new Stack<>();
 
-            if(cnt < 0) return false;
-        } 
+        for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    st.push(1);
+                } else if (s.charAt(i) == ')') {
+                    if (st.isEmpty()) {
+                        answer = false;
+                        break;
+                    } else {
+                        st.pop();
+                    }
+                }
+            }
 
-        if(cnt != 0 || s.length() == 0)
-            answer = false;
+            if(!st.isEmpty()) {
+                answer = false;
+            }
 
+            System.out.println(res);
         return answer;
-    }
+    }   
+
 }
