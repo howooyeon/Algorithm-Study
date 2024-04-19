5105aab0be004ba83173d8706de292de6d04bce2
hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2024-04-19 21:27:56 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 74.4 MB, 시간: 0.02 ms
+메모리: 73.1 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 19일 21:25:03
+2024년 04월 19일 21:27:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int x = 1;
+        int y = 1;
         while(true){
-            int y = brown/2 - x + 2;
+            int x = brown/2 - y + 2;
                 if(x * y - brown == yellow){
-                    return new int[] {y, x};
+                    return new int[] {x, y};
                 }
-            x++;
+            y++;
         }
     }
 }
