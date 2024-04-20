cd4416daf3991f0e500a3f454febd4a53cf757b1
hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2024-04-21 03:14:13 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,13 +1,11 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int y = 1;
-        while(true){
+        for(int y = 0; ; y++) {
             int x = brown/2 - y + 2;
                 if(x * y - brown == yellow){
                     return new int[] {x, y};
                 }
-            y++;
+            }
         }
-    }
 }
