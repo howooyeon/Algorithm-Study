13c443dd618e543ad3e71516e5049dc47606c3c7
Hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2025-08-24 20:43:02 +0900

    [level 2] Title: [3차] n진수 게임, Time: 412.72 ms, Memory: 382 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(int n, int t, int m, int p) {        
+        int len = m * (t - 1) + p;
+        String s = "";
+        int num = 0;
+        
+        while(s.length() < len){
+            s += Integer.toString(num,n).toUpperCase();
+            num++;
+        }
+        
+        String answer = "";
+        int idx = 0;
+        while(true){
+            answer += s.charAt(idx * m + p - 1);    
+            idx++;
+            if(idx == t) break;
+        }
+        return answer;
+    }
+}
