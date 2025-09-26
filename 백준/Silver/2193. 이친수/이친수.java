2b6f4fa029b359505247b3a099a77f8cea7b42fe
Hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2025-09-26 16:57:20 +0900

    [Silver III] Title: 이친수, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+	static int n;
+	static long dp[];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		n = Integer.parseInt(br.readLine());
+        dp = new long[n + 1];
+
+		dp[0] = 0;
+		dp[1] = 1;
+		
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		System.out.println(dp[n]);
+	}
+}
