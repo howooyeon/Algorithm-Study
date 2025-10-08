e7d4b715ab729ea11bc641aa3cf69fd2ca6b4ece
Hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2025-10-09 02:29:49 +0900

    [Gold V] Title: 1로 만들기 2, Time: 124 ms, Memory: 21964 KB -BaekjoonHub

---
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+        int[] prev = new int[N + 1]; // 경로 추적용
+
+        dp[1] = 0;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;  // 1을 뺀 경우 + 1
+            prev[i] = i - 1;
+
+            if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+                dp[i] = dp[i / 2] + 1;
+                prev[i] = i / 2;
+            }
+            if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+                dp[i] = dp[i / 3] + 1;
+                prev[i] = i / 3;
+            }
+        }
+
+        System.out.println(dp[N]);
+
+        // 경로
+        StringBuilder sb = new StringBuilder();
+        int cur = N;
+        while (cur > 0) {
+            sb.append(cur).append(" ");
+            cur = prev[cur];
+            if (cur == 0) break;
+        }
+        System.out.println(sb);
+    }
+}
