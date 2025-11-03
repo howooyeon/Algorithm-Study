336a4f4ebcb78f7d5d7233c59088d1c1ec055557
Hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2025-11-04 01:05:08 +0900

    [Silver IV] Title: ATM, Time: 216 ms, Memory: 20324 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] p = new int[n];
+        for (int i = 0; i < n; i++)
+            p[i] = sc.nextInt();
+
+        Arrays.sort(p);
+
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += p[i] * (n - i);
+
+        System.out.println(sum);
+    }
+}
